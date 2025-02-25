2ee3705772db08a7bbd04506f0e6dacce6826e40
Giovannise <giovannise_lisboa@hotmail.com>
2025-02-25 09:10:20 -0300

    Case2_login-valido-e-ajuste-padroes-de-projeto

---
+++ support/db.js
@@ -0,0 +1,26 @@
+import pgPromise from 'pg-promise'
+
+const pgp = pgPromise()
+// postgresql = servidor de banco de dados
+//dba = usuário
+// dba = senha
+//paybank-db = nome do container
+//5432 = porta
+//UserDb = nome do banco
+//e todas essas informações estão no arquivo docker-compose.yml
+const db = pgp('postgresql://dba:dba@paybank-db:5432/UserDB')
+
+//função assíncrona, pois fica "esperando" uma resposta enquanto executa
+//export, pois preciso importar ela lá no meu teste
+export async function obterCodigo2FA() {
+    // cria uma variável query que consulta no banco:
+    // SELECT code (ou seja, o código de 2 fatores enviado por email)
+	// FROM public."TwoFactorCode"
+	// ORDER BY id DESC (ordena do último recebido até o primeiro)
+	// LIMIT 1; (pega somente 1 resultado, ou seja, o mais recente)
+    const query = 'SELECT code FROM public."TwoFactorCode" ORDER BY id DESC LIMIT 1;'
+
+    //cria uma constante que recebe 1 ou nenhum resultado da query, que também é o retorno da função
+    const result = await db.oneOrNone(query)
+    return result.code
+}
--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,25 +1,52 @@
 import { test, expect } from '@playwright/test';
 
+import { obterCodigo2FA } from '../support/db';
+
+import { LoginPage } from '../pages/LoginPage';
+
+import ( DashPage ) from '../pages/DashPage';
+
 test('Não deve logar quando o código de autenticação é inválido', async ({ page }) => {
+
+  const loginPage = new LoginPage(page)
 
   const user = {
     cpf: '00000014141',
     senha: '147258'
   }
 
-  await page.goto('http://paybank-mf-auth:3000/');
-
-  await page.getByRole('textbox', { name: 'Digite seu CPF' }).fill(user.cpf);
-  await page.getByRole('button', { name: 'Continuar' }).click();
-
-  
-  for (const digito of user.senha) {
-    await page.getByRole('button', { name: digito }).click();
-  }
-  await page.getByRole('button', { name: 'Continuar' }).click();
+  await loginPage.acessaPagina()
+  await loginPage.informaCpf(user.cpf)
+  await loginPage.informaSenha(user.senha)
   
   await page.getByRole('textbox', { name: '000000' }).fill('123456');
   await page.getByRole('button', { name: 'Verificar' }).click();
 
   await expect(page.locator('span')).toContainText('Código inválido. Por favor, tente novamente.');
+});
+
+test('Deve acessar a conta do usuário', async ({ page }) => {
+
+  const loginPage = new LoginPage(page)
+  const dashPage = new DashPage(page)
+
+  const user = {
+    cpf: '00000014141',
+    senha: '147258'
+  }
+
+  await loginPage.acessaPagina()
+  await loginPage.informaCpf(user.cpf)
+  await loginPage.informaSenha(user.senha)
+
+  //temporário
+  await page.waitForTimeout(3000);
+
+  const code = await obterCodigo2FA()
+  await loginPage.informa2FA(code)
+  
+  //temporário
+  await page.waitForTimeout(2000)
+  
+  expect(await dashPage.obterSaldo()).toHaveText('R$ 5.000,00')
 });
